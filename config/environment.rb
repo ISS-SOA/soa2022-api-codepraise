466a1f955fcc9b0798fc017d86d38f9f9dba6e98
Soumya Ray <soumya.ray@gmail.com>
2021-12-06 08:03:57 +0800

    reverse-proxy caching of project appraisal

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,8 @@ require 'logger'
 require 'rack/session'
 require 'roda'
 require 'sequel'
-# require 'delegate' # needed until Rack 2.3 fixes delegateclass bug
+require 'rack/cache'
+require 'redis-rack-cache'
 
 module CodePraise
   # Environment-specific configuration
@@ -20,20 +21,36 @@ module CodePraise
     Figaro.load
     def self.config = Figaro.env
 
+    # Setup Cacheing mechanism
+    configure :development do
+      use Rack::Cache,
+          verbose: true,
+          metastore: 'file:_cache/rack/meta',
+          entitystore: 'file:_cache/rack/body'
+    end
+
+    configure :production do
+      use Rack::Cache,
+          verbose: true,
+          metastore: "#{config.REDISCLOUD_URL}/0/metastore",
+          entitystore: "#{config.REDISCLOUD_URL}/0/entitystore"
+    end
+
+    # Automated HTTP stubbing for testing only
     configure :app_test do
       require_relative '../spec/helpers/vcr_helper'
       VcrHelper.setup_vcr
       VcrHelper.configure_vcr_for_github(recording: :none)
     end
 
-    # Database Setup
+    # Database Setup (ensure DATABASE_URL already configured on production)
     configure :development, :test, :app_test do
       require 'pry'; # for breakpoints
       ENV['DATABASE_URL'] = "sqlite://#{config.DB_FILENAME}"
     end
 
     DB = Sequel.connect(ENV.fetch('DATABASE_URL'))
-    # deliberately :reek:UncommunicativeMethodName calling method DB
+    # deliberately ignore :reek:UncommunicativeMethodName calling method DB
     def self.DB = DB # rubocop:disable Naming/MethodName
 
     # Logger Setup
